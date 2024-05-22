a41dbc331a1e74097566bf60940b64dd4fff6305
JinLong <2593521486@qq.com>
2024-05-22 13:53:25 +0800

    test

--- MDK-ARM/RTK/tasks.d
+++ MDK-ARM/RTK/tasks.d
@@ -1,9 +1,9 @@
 rtk\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/tasks.c
-rtk\tasks.o: D:\Keil\ARM\ARMCC\Bin\..\include\stdlib.h
-rtk\tasks.o: D:\Keil\ARM\ARMCC\Bin\..\include\string.h
+rtk\tasks.o: D:\MDK5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdlib.h
+rtk\tasks.o: D:\MDK5\ARM\ARM_Compiler_5.06u7\Bin\..\include\string.h
 rtk\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
-rtk\tasks.o: D:\Keil\ARM\ARMCC\Bin\..\include\stddef.h
-rtk\tasks.o: D:\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+rtk\tasks.o: D:\MDK5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stddef.h
+rtk\tasks.o: D:\MDK5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
 rtk\tasks.o: ../Core/Inc/FreeRTOSConfig.h
 rtk\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
 rtk\tasks.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
